fa5c2723eb680780261a9b9db75f4d4c38648fe8
ToniCoffee <96419343+ToniCoffee@users.noreply.github.com>
2021-12-22 20:24:52 +0100

    Add files via upload

---
+++ tetrisPieceTest.js
@@ -0,0 +1,97 @@
+/* const entity1 = piece(100, 100, 100, 120, 100, 140, 120, 140, 'orange');
+entity1.rotationLeft = {
+  zero: {x1: -20, y1: -20, x2: 0, y2: 0, x3: 20, y3: 20, x4: 40, y4: 0 },
+  noventa: {x1: 20, y1: -20, x2: 0, y2: 0, x3: -20, y3: 20, x4: 0, y4: 40 },
+  cientoOchenta: {x1: 20, y1: 20, x2: 0, y2: 0, x3: -20, y3: -20, x4: -40, y4: 0 },
+  doscientosSetenta: {x1: -20, y1: 20, x2: 0, y2: 0, x3: 20, y3: -20, x4: 0, y4: -40 }
+};
+entity1.rotationRight = {
+  zero: {x1: 20, y1: -20, x2: 0, y2: 0, x3: -20, y3: 20, x4: 0, y4: 40 },
+  noventa: {x1: 20, y1: 20, x2: 0, y2: 0, x3: -20, y3: -20, x4: -40, y4: 0 },
+  cientoOchenta: {x1: -20, y1: 20, x2: 0, y2: 0, x3: 20, y3: -20, x4: 0, y4: -40 },
+  doscientosSetenta: {x1: -20, y1: -20, x2: 0, y2: 0, x3: 20, y3: 20, x4: 40, y4: 0 }
+}; */
+
+/* const entity2 = piece(100, 300, 100, 320, 100, 340, 80, 340, 'blue');
+entity2.rotationLeft = {
+  zero: {x1: -20, y1: -20, x2: 0, y2: 0, x3: 20, y3: 20, x4: 0, y4: 40 },
+  noventa: {x1: 20, y1: -20, x2: 0, y2: 0, x3: -20, y3: 20, x4: -40, y4: 0 },
+  cientoOchenta: {x1: 20, y1: 20, x2: 0, y2: 0, x3: -20, y3: -20, x4: 0, y4: -40 },
+  doscientosSetenta: {x1: -20, y1: 20, x2: 0, y2: 0, x3: 20, y3: -20, x4: 40, y4: 0 }
+};
+entity2.rotationRight = {
+  zero: {x1: 20, y1: -20, x2: 0, y2: 0, x3: -20, y3: 20, x4: -40, y4: 0 },
+  noventa: {x1: 20, y1: 20, x2: 0, y2: 0, x3: -20, y3: -20, x4: 0, y4: -40 },
+  cientoOchenta: {x1: -20, y1: 20, x2: 0, y2: 0, x3: 20, y3: -20, x4: 40, y4: 0 },
+  doscientosSetenta: {x1: -20, y1: -20, x2: 0, y2: 0, x3: 20, y3: 20, x4: 0, y4: 40 }
+}; */
+
+/* const entity3 = piece(140, 100, 160, 100, 140, 120, 160, 120, 'yellow');
+entity3.rotationLeft = {
+  zero: {x1: 0, y1: 0, x2: 0, y2: 0, x3: 0, y3: 0, x4: 0, y4: 0 },
+  noventa: {x1: 0, y1: 0, x2: 0, y2: 0, x3: 0, y3: 0, x4: 0, y4: 0 },
+  cientoOchenta: {x1: 0, y1: 0, x2: 0, y2: 0, x3: 0, y3: 0, x4: 0, y4: 0 },
+  doscientosSetenta: {x1: 0, y1: 0, x2: 0, y2: 0, x3: 0, y3: 0, x4: 0, y4: 0 }
+};
+entity3.rotationRight = {
+  zero: {x1: 0, y1: 0, x2: 0, y2: 0, x3: 0, y3: 0, x4: 0, y4: 0 },
+  noventa: {x1: 0, y1: 0, x2: 0, y2: 0, x3: 0, y3: 0, x4: 0, y4: 0 },
+  cientoOchenta: {x1: 0, y1: 0, x2: 0, y2: 0, x3: 0, y3: 0, x4: 0, y4: 0 },
+  doscientosSetenta: {x1: 0, y1: 0, x2: 0, y2: 0, x3: 0, y3: 0, x4: 0, y4: 0 }
+}; */
+
+/* const entity4 = piece(220, 100, 220, 120, 220, 140, 220, 160, 'lightblue');
+entity4.rotationLeft = {
+  zero: {x1: -20, y1: -20, x2: 0, y2: 0, x3: 20, y3: 20, x4: 40, y4: 40 },
+  noventa: {x1: 20, y1: -40, x2: 0, y2: -20, x3: -20, y3: 0, x4: -40, y4: 20 },
+  cientoOchenta: {x1: 40, y1: 40, x2: 20, y2: 20, x3: 0, y3: 0, x4: -20, y4: -20 },
+  doscientosSetenta: {x1: -40, y1: 20, x2: -20, y2: 0, x3: 0, y3: -20, x4: 20, y4: -40 }
+};
+entity4.rotationRight = {
+  zero: {x1: 40, y1: -20, x2: 20, y2: 0, x3: 0, y3: 20, x4: -20, y4: 40 },
+  noventa: {x1: 20, y1: 20, x2: 0, y2: 0, x3: -20, y3: -20, x4: -40, y4: -40 },
+  cientoOchenta: {x1: -20, y1: 40, x2: 0, y2: 20, x3: 20, y3: 0, x4: 40, y4: -20 },
+  doscientosSetenta: {x1: -40, y1: -40, x2: -20, y2: -20, x3: 0, y3: 0, x4: 20, y4: 20 }
+}; */
+
+/* const entity5 = piece(100, 200, 120, 200, 120, 220, 140, 220, 'red');
+entity5.rotationLeft = {
+  zero: {x1: -20, y1: 0, x2: 0, y2: -20, x3: 20, y3: 0, x4: 40, y4: -20 },
+  noventa: {x1: -20, y1: -20, x2: 0, y2: 0, x3: -20, y3: 20, x4: 0, y4: 40 },
+  cientoOchenta: {x1: 40, y1: -20, x2: 20, y2: 0, x3: 0, y3: -20, x4: -20, y4: 0 },
+  doscientosSetenta: {x1: 0, y1: 40, x2: -20, y2: 20, x3: 0, y3: 0, x4: -20, y4: -20 }
+};
+entity5.rotationRight = {
+  zero: {x1: 0, y1: -40, x2: 20, y2: -20, x3: 0, y3: 0, x4: 20, y4: 20 },
+  noventa: {x1: 20, y1: 0, x2: 0, y2: 20, x3: -20, y3: 0, x4: -40, y4: 20 },
+  cientoOchenta: {x1: 20, y1: 20, x2: 0, y2: 0, x3: 20, y3: -20, x4: 0, y4: -40 },
+  doscientosSetenta: {x1: -40, y1: 20, x2: -20, y2: 0, x3: 0, y3: 20, x4: 20, y4: 0 }
+}; */
+
+/* const entity6 = piece(100, 400, 120, 400, 120, 380, 140, 380, 'green');
+entity6.rotationLeft = {
+  zero: {x1: -20, y1: 20, x2: 0, y2: 0, x3: -20, y3: -20, x4: 0, y4: -40 },
+  noventa: {x1: -20, y1: 0, x2: 0, y2: 20, x3: 20, y3: 0, x4: 40, y4: 20 },
+  cientoOchenta: {x1: 0, y1: -40, x2: -20, y2: -20, x3: 0, y3: 0, x4: -20, y4: 20 },
+  doscientosSetenta: {x1: 40, y1: 20, x2: 20, y2: 0, x3: 0, y3: 20, x4: -20, y4: 0 }
+};
+entity6.rotationRight = {
+  zero: {x1: -40, y1: -20, x2: -20, y2: 0, x3: 0, y3: -20, x4: 20, y4: 0 },
+  noventa: {x1: 20, y1: -20, x2: 0, y2: 0, x3: 20, y3: 20, x4: 0, y4: 40 },
+  cientoOchenta: {x1: 20, y1: 0, x2: 0, y2: -20, x3: -20, y3: 0, x4: -40, y4: -20 },
+  doscientosSetenta: {x1: 0, y1: 40, x2: 20, y2: 20, x3: 0, y3: 0, x4: 20, y4: -20 }
+}; */
+
+/* const entity7 = piece(240, 400, 260, 400, 280, 400, 260, 420, 'darkviolet');
+entity7.rotationLeft = {
+  zero: {x1: -20, y1: 20, x2: 0, y2: 0, x3: 20, y3: -20, x4: 20, y4: 20 },
+  noventa: {x1: -20, y1: -20, x2: 0, y2: 0, x3: 20, y3: 20, x4: -20, y4: 20 },
+  cientoOchenta: {x1: 20, y1: -20, x2: 0, y2: 0, x3: -20, y3: 20, x4: -20, y4: -20 },
+  doscientosSetenta: {x1: 20, y1: 20, x2: 0, y2: 0, x3: -20, y3: -20, x4: 20, y4: -20 }
+};
+entity7.rotationRight = {
+  zero: {x1: -20, y1: -20, x2: 0, y2: 0, x3: 20, y3: 20, x4: -20, y4: 20 },
+  noventa: {x1: 20, y1: -20, x2: 0, y2: 0, x3: -20, y3: 20, x4: -20, y4: -20 },
+  cientoOchenta: {x1: 20, y1: 20, x2: 0, y2: 0, x3: -20, y3: -20, x4: 20, y4: -20 },
+  doscientosSetenta: {x1: -20, y1: 20, x2: 0, y2: 0, x3: 20, y3: -20, x4: 20, y4: 20 }
+}; */
